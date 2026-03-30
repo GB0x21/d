343d470fa041aa2f45f8eb5323af938c36c1a948
GB0x21 <gabriel@insultechs.com>
2026-03-29 19:33:32 -0700

    Update store_locator.py

--- bot/store_locator.py
+++ bot/store_locator.py
@@ -13,27 +13,26 @@ STORE_NUMBER_PATTERNS = [
 # California ZIP codes (start with 9)
 ZIP_PATTERN = re.compile(r"\b(9[0-9]{4})\b")
 
-# Known Bay Area Home Depot stores
+# Known Bay Area Home Depot stores (user's target stores)
 BAY_AREA_STORES = {
-    "0601": {"name": "Colma", "address": "150 Colma Blvd, Colma, CA 94014"},
-    "0611": {"name": "San Francisco", "address": "2 Bayshore Blvd, San Francisco, CA 94124"},
-    "1009": {"name": "Emeryville", "address": "3838 Hollis St, Emeryville, CA 94608"},
-    "1011": {"name": "Dublin", "address": "6000 Dublin Blvd, Dublin, CA 94568"},
-    "1012": {"name": "San Jose - Almaden", "address": "635 W Capitol Expy, San Jose, CA 95136"},
-    "1019": {"name": "Concord", "address": "1975 Diamond Blvd, Concord, CA 94520"},
-    "1021": {"name": "Sunnyvale", "address": "811 E Arques Ave, Sunnyvale, CA 94085"},
-    "1025": {"name": "San Jose - Story", "address": "2181 Monterey Rd, San Jose, CA 95112"},
-    "1038": {"name": "Fremont", "address": "39100 Argonaut Way, Fremont, CA 94538"},
-    "1040": {"name": "Mountain View", "address": "2450 Charleston Rd, Mountain View, CA 94043"},
-    "1044": {"name": "Oakland", "address": "4000 Alameda Ave, Oakland, CA 94601"},
-    "1049": {"name": "Hayward", "address": "25700 Clawiter Rd, Hayward, CA 94545"},
-    "1050": {"name": "San Mateo", "address": "2001 Bridgepointe Pkwy, San Mateo, CA 94404"},
-    "1053": {"name": "Richmond", "address": "5401 Central Ave, Richmond, CA 94804"},
-    "1056": {"name": "Walnut Creek", "address": "2000 N Main St, Walnut Creek, CA 94596"},
-    "1060": {"name": "Milpitas", "address": "301 Ranch Dr, Milpitas, CA 95035"},
-    "1062": {"name": "Redwood City", "address": "2303 Broadway, Redwood City, CA 94063"},
-    "1067": {"name": "Gilroy", "address": "6975 Camino Arroyo, Gilroy, CA 95020"},
-    "1082": {"name": "Livermore", "address": "4255 First St, Livermore, CA 94551"},
+    "0625": {"name": "San Leandro", "address": "1933 Davis St, San Leandro, CA 94577"},
+    "0627": {"name": "Emeryville", "address": "3838 Hollis St, Emeryville, CA 94608"},
+    "0631": {"name": "Vallejo", "address": "401 Columbus Pkwy, Vallejo, CA 94591"},
+    "0633": {"name": "San Rafael", "address": "5800 Northgate Dr, San Rafael, CA 94903"},
+    "0634": {"name": "Concord", "address": "1975 Diamond Blvd, Concord, CA 94520"},
+    "0637": {"name": "Fairfield", "address": "4599 Central Way, Fairfield, CA 94534"},
+    "0638": {"name": "Fremont", "address": "39100 Argonaut Way, Fremont, CA 94538"},
+    "0639": {"name": "Dublin", "address": "6000 Dublin Blvd, Dublin, CA 94568"},
+    "0644": {"name": "Pittsburg", "address": "4527 Century Blvd, Pittsburg, CA 94565"},
+    "1007": {"name": "Oakland", "address": "4000 Alameda Ave, Oakland, CA 94601"},
+    "1015": {"name": "El Cerrito", "address": "6000 Potrero Ave, El Cerrito, CA 94530"},
+    "1017": {"name": "Hayward", "address": "25700 Clawiter Rd, Hayward, CA 94545"},
+    "1044": {"name": "Hercules", "address": "340 Sycamore Ave, Hercules, CA 94547"},
+    "1045": {"name": "Napa", "address": "205 Soscol Ave, Napa, CA 94559"},
+    "1076": {"name": "Brentwood", "address": "2501 Sand Creek Rd, Brentwood, CA 94513"},
+    "1380": {"name": "Martinez", "address": "920 Arnold Dr, Martinez, CA 94553"},
+    "6604": {"name": "San Ramon", "address": "2250 Camino Ramon, San Ramon, CA 94583"},
+    "6641": {"name": "Livermore", "address": "4255 First St, Livermore, CA 94551"},
 }
 
 # Bay Area ZIP code ranges (approximate)
